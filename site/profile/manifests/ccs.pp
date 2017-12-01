a4fba0679ca56b74e12a069b33cf1dd435bda11b
Tony Johnson <tonyj@stanford.edu>
2017-12-01 23:12:06 +0000

    Add new templates for files in /etc/ccs

--- site/profile/manifests/ccs.pp
+++ site/profile/manifests/ccs.pp
@@ -1,8 +1,27 @@
 class profile::ccs {
-   file { ['/lsst', '/lsst/ccs', '/lsst/ccsadmin', '/lsst/ccsadmin/package-lists', '/var/log/ccs', '/etc/ccs' ] : 
+   file { '/var/log/ccs' : 
+      ensure => directory,
+      mode => '1764',
+      owner => ccs, 
+      group => ccs,
+   }
+
+   file { ['/lsst', '/lsst/ccs', '/lsst/ccsadmin', '/lsst/ccsadmin/package-lists', '/etc/ccs' ] : 
       ensure => directory,
       mode => '1764',
    }
+
+   file { '/etc/ccs/ccsGlobal.properties' :
+      ensure => file,
+      mode => '1764',
+      content => epp('profile/ccsGlobal.properties.epp'),       
+   }   
+
+   file { '/etc/ccs/udp_ccs.properties' :
+      ensure => file,
+      mode => '1764',
+      content => epp('profile/udp_ccs.properties.epp'),       
+   }   
 
    user { 'ccs':
       ensure => present,
