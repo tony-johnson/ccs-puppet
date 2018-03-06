399a11a0be262d768423cf01fc229dcfbe6819c7
root <root@puppermaster.loocaldomain>
2018-03-05 21:00:53 -0500

    Fixing dependencies

--- site/profile/manifests/java.pp
+++ site/profile/manifests/java.pp
@@ -3,6 +3,7 @@ java::oracle { 'jdk8' :
   ensure  => 'present',
   version_major => '8u161',
   version_minor => 'b12',
+  url_hash => '2f38c3b165be4555a1fa6e98c45e0808',
   java_se => 'jdk', 
   before => Exec['update-java-alternatives']
 }
