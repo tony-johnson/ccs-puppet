69641c39b766b154b13ad022f5ba409dfeb6af0d
Tony Johnson <tonyj@stanford.edu>
2018-03-11 13:48:23 -0700

    Split subsystem into multiple calls to dist

--- site/profile/manifests/ccs/subsystem.pp
+++ site/profile/manifests/ccs/subsystem.pp
@@ -1,17 +1,7 @@
 class profile::ccs::subsystem (String $subsystemName, String $version, String $command, String $service) {
 
-   include profile::ccs::ccs
-   include profile::ccs::installscript
-   include profile::ccs::java
-
-   file { '/lsst/ccsadmin/package-lists/ccsApplications.txt' :
-     ensure => file,
-     content => "[ccs]\norg-lsst-ccs-subsystem-${subsystemName}-main = ${version}\norg-lsst-ccs-subsystem-${subsystemName}-gui = ${version}\nexecutable.${command} = org-lsst-ccs-subsystem-demo-main\nexecutable.CCS-Console = org-lsst-ccs-subsystem-${subsystemName}-gui\nexecutable.ShellCommandConsole = org-lsst-ccs-subsystem-${subsystemName}-gui\n",
-   }
-
-   exec { 'doit':
-     command => '/lsst/ccsadmin/release/bin/install.py --ccs_inst_dir /lsst/ccs/prod /lsst/ccsadmin/package-lists/ccsApplications.txt',
-   }
+   profile::ccs::dist("org-lsst-ccs-subsystem-${subsystemName}-main", $version, [$command])
+   profile::ccs::dist("org-lsst-ccs-subsystem-${subsystemName}-gui", $version, ["CCS-Console", "ShellCommandConsole")
 
    file { "/lib/systemd/system/${service}.service":
      mode    => '0644',
