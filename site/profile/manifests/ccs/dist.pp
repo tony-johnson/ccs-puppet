3f0933c0df8e72f0b1684e066c4d12311afeb152
Tony Johnson <tonyj@stanford.edu>
2018-03-11 14:12:02 -0700

    Split subsystem into multiple calls to dist

--- site/profile/manifests/ccs/dist.pp
+++ site/profile/manifests/ccs/dist.pp
@@ -9,13 +9,13 @@ class profile::ccs::dist (String $distfile, String $version, $commands = []) {
 
    file { "/lsst/ccsadmin/package-lists/${distfile}.txt":
      ensure => file,
-     content => "[ccs]\n${distfile} = ${version}"
+     content => "[ccs]\n${distfile} = ${version}",
    }
    
    $commands.each |String $command| {
      file-line { "append":
         path => "/lsst/ccsadmin/package-lists/${distfile}.txt",
-        line => "executable.${command} = ${distfile}
+        line => "executable.${command} = ${distfile}",
      }
    }
 
