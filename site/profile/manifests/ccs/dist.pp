4dbe89ba4114d573901b9bc2492a9e4dbcc6ade9
Tony Johnson <tonyj@stanford.edu>
2018-03-11 14:02:34 -0700

    Split subsystem into multiple calls to dist

--- site/profile/manifests/ccs/dist.pp
+++ site/profile/manifests/ccs/dist.pp
@@ -7,19 +7,19 @@ class profile::ccs::dist (String $distfile, String $version, $commands = []) {
    include profile::ccs::installscript
    include profile::ccs::java
 
-   file { "lsst/ccsadmin/package-lists/${distfile}.txt":
+   file { "/lsst/ccsadmin/package-lists/${distfile}.txt":
      ensure => file,
      content => "[ccs]\n${distfile} = ${version}"
    }->
    $commands.each |String $command| {
      file-line { "append":
-        path => "lsst/ccsadmin/package-lists/${distfile}.txt",
+        path => "/lsst/ccsadmin/package-lists/${distfile}.txt",
         line => "executable.${command} = ${distfile}
      }
    }
 
    exec { 'doit':
-     command => "lsst/ccsadmin/release/bin/install.py --ccs_inst_dir /lsst/ccs/prod lsst/ccsadmin/package-lists/${distfile}.txt"
+     command => "/lsst/ccsadmin/release/bin/install.py --ccs_inst_dir /lsst/ccs/prod /lsst/ccsadmin/package-lists/${distfile}.txt"
    }
 
 }
